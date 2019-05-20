d50e3972d1e557f934f79171ecd765634fc90bc6
elijahmm17 <elijahmm17@gmail.com>
2019-05-20 21:17:36 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song 
+  attr_reader :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre
+    @@count += 1 
+    @@artists << artist
+    @@genres << genre
+  end 
+  def count 
+    @@count 
+  end 
+  def artists 
+    results = []
+    @@artists.each do |a|
+      results << a if !results.include?(a)
+    end 
+    results
+  end 
+end 
